2c7a0365c680ea1d89da6fc0475e00c807836e29
imdba <261726511@qq.com>
2015-07-13 18:10:43 +0800

    add by zhanghao

--- Array/array_map.php
+++ Array/array_map.php
@@ -16,4 +16,15 @@ $rs = [
 $str = implode(',',array_map('reset',$rs));
 echo $str;
 
+function myfunction($v)
+{
+    if ($v==="Dog")
+    {
+        return "Fido";
+    }
+    return $v;
+}
+$a=array("Horse","Dog","Cat");
+print_r(array_map("myfunction",$a));
+
 //3,4
